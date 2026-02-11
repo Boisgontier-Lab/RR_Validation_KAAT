fe2e7a6ea110904d18dabc9eeacc9f2299ec0ded
KaynePark <119626595+KaynePark@users.noreply.github.com>
2026-02-11 13:29:37 -0500

    Delete findMaxSpeed.m

--- findMaxSpeed.m
+++
@@ -1,5 +0,0 @@
-function [maxSpeed maxSpeedTime] = findMaxSpeed(speed)
-%get maximum speed and time at maximum speed
-
-[maxSpeedTemp, maxSpeedTime] = max(speed);
-maxSpeed = round(maxSpeedTemp*100);
